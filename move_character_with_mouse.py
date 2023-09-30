80f093d4330be3a9ea473a426ba7a8a4e0747212
yallmoong <victoria0404@naver.com>
2023-10-01 06:01:08 +0900

    캐릭터 손위치로 이동 완성

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -1,8 +1,10 @@
 from pico2d import *
 import random
+import math
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
+r = 10
 
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
@@ -20,13 +22,14 @@ def handle_events():
     pass
 
 def creat_hand():
-    global hand_coord
+    global hand_coord, character_coord, angle, value
     hand_coord = (random.randint(0, TUK_WIDTH - 50), random.randint(0, TUK_HEIGHT - 52))
+    angle = math.atan2(hand_coord[1] - character_coord[1], hand_coord[0] - character_coord[0])
+    value = (math.cos(angle) * r, math.sin(angle) * r)
 
 running = True
-character_coord = (TUK_WIDTH // 2, TUK_HEIGHT // 2)
-hand_coord = (0, 0)
-frame = 0
+character_coord, hand_coord, value = (TUK_WIDTH // 2, TUK_HEIGHT // 2), (0, 0), (0, 0)
+frame, angle = 0, 0
 hide_cursor()
 creat_hand()
 
@@ -37,11 +40,22 @@ while running:
     character.clip_draw(frame * 100, 100 * 1, 100, 100, character_coord[0], character_coord[1])
     frame = (frame + 1) % 8
 
+    character_coord = (character_coord[0] + value[0], character_coord[1] + value[1])
+    if value[0] > 0 and value[1] > 0 and character_coord[0] >= hand_coord[0] and character_coord[1] >= hand_coord[1]:
+        creat_hand()
+    elif value[0] <= 0 and value[1] > 0 and character_coord[0] <= hand_coord[0] and character_coord[1] >= hand_coord[1]:
+        creat_hand()
+    elif value[0] <= 0 and value[1] <= 0 and character_coord[0] <= hand_coord[0] and character_coord[1] <= hand_coord[1]:
+        creat_hand()
+    elif value[0] > 0 and value[1] <= 0 and character_coord[0] >= hand_coord[0] and character_coord[1] <= hand_coord[1]:
+        creat_hand()
+
     update_canvas()
     handle_events()
     delay(0.05)
 
 close_canvas()
+
 
 
 

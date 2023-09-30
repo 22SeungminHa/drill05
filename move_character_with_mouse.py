b7508b1a330658632eb23180c6d268a70eaa7ec5
yallmoong <victoria0404@naver.com>
2023-10-01 05:06:01 +0900

    handle_events() 함수 수정

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -5,19 +5,17 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
-
+hand_arrow = load_image('hand_arrow.png')
 
 def handle_events():
     global running
-    global x, y
+    # global x, y
     events = get_events()
     for event in events:
-        if event.type == SDL_QUIT:
+        if (event.type == SDL_QUIT) or (event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE):
             running = False
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
+        # elif event.type == SDL_MOUSEMOTION:
+        #     x, y = event.x, TUK_HEIGHT - 1 - event.y
     pass
 
 
